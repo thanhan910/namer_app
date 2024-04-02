c69a66e61b586e05bdba685b222970b4c14dbcba
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-02 18:20:04 +1100

    Add a simple button

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,14 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random AWESOME idea:'), // ← Example change.
+          Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
