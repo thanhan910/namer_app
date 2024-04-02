29d6054f8902a47be7c52482ebb0a985efc1bc39
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-02 18:38:12 +1100

    Only extract the current word instead of the entire appState

--- lib/main.dart
+++ lib/main.dart
@@ -54,12 +54,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),
           ElevatedButton(
             onPressed: () {
               print('button pressed!');
