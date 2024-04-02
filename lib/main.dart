7eeb26414a11a90187dd8885aa85579b54b8a248
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-02 18:23:24 +1100

    Add getNext() method to update current word pair

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
