907f3105d3dab873a9990707723b62834c580419
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-04-03 11:36:59 +1100

    Refactor MyHomePage to use StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,12 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
